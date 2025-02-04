6911de3ffee60c7c12a17fc2b55b9efe4928e3dc
Vaidotas Piekus <vaidotasp@gmail.com>
2025-02-03 20:58:18 -0500

    hash-object

--- cmd/mygit/main.go
+++ cmd/mygit/main.go
@@ -3,12 +3,24 @@ package main
 import (
 	"bytes"
 	"compress/zlib"
+	"crypto/sha1"
 	"fmt"
 	"io"
 	"log"
 	"os"
 	"path/filepath"
+	"strconv"
 )
+
+func generateSHA1(uncompressed_content []byte) string {
+	header := []byte("blob ")
+	sizeStr := strconv.Itoa(len(uncompressed_content))
+	header = append(header, []byte(sizeStr)...)
+	header = append(header, 0)
+	header = append(header, uncompressed_content...)
+	hash := sha1.Sum(header)
+	return fmt.Sprintf("%x", hash)
+}
 
 // Usage: your_program.sh <command> <arg1> <arg2> ...
 func main() {
@@ -89,6 +101,71 @@ func main() {
 			decompressedString := string(byte_output[:])
 			fmt.Print(decompressedString)
 		}
+	case "hash-object":
+		args := os.Args
+		path_cmd := args[2]
+
+		// check that we have -p flag and it is followed by 40 length blob sha
+		if path_cmd != "-w" {
+			fmt.Fprintf(os.Stderr, "Missing -w flag or incorrect Blob SHA")
+			os.Exit(1)
+		}
+
+		input_file := args[3]
+		data, err := os.ReadFile(input_file) // For read access.
+		if err != nil {
+			log.Fatal(err)
+		}
+
+		var buf bytes.Buffer
+		w := zlib.NewWriter(&buf)
+		w.Write([]byte(data))
+		w.Close()
+
+		r, err := zlib.NewReader(&buf)
+		if err != nil {
+		}
+		defer r.Close()
+
+		var uncompressed bytes.Buffer
+		_, err = io.Copy(&uncompressed, r)
+		if err != nil {
+			log.Fatal(err)
+		}
+
+		hash := generateSHA1(data)
+		dir_name := hash[:2]
+		file_name := hash[2:]
+
+		dir_path, err := filepath.Abs(".git/objects/")
+		if err != nil {
+			fmt.Println("Error reading: ", err.Error())
+		}
+
+		if err := os.MkdirAll(dir_path+"/"+dir_name, 0755); err != nil {
+			fmt.Fprintf(os.Stderr, "Error creating directory: %s\n", err)
+		}
+
+		//writing file
+		file_path := dir_path + "/" + dir_name + "/" + file_name
+		sizeStr := strconv.Itoa(len(data))
+		output := []byte("blob ")
+		uncompressed_file_content := uncompressed.String()
+		output = append(output, []byte(sizeStr)...)
+		output = append(output, 0)
+		output = append(output, uncompressed_file_content...)
+
+		var b bytes.Buffer
+		w = zlib.NewWriter(&b)
+		w.Write([]byte(output))
+		w.Close()
+
+		err = os.WriteFile(file_path, b.Bytes(), 0644)
+		if err != nil {
+			fmt.Println("Error writing: ", err.Error())
+		}
+
+		fmt.Print(hash)
 	default:
 		fmt.Fprintf(os.Stderr, "Unknown command %s\n", command)
 		os.Exit(1)
